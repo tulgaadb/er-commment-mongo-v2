4ae50b1566ce50df588871f1d54c08fd9f3eb6ef
tulgaadb <130822314+tulgaadb@users.noreply.github.com>
2023-11-10 12:28:19 +0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>
+          NextJs 13 Starter Kit (TypeScript)
+        </h1>
+      </div>
+    </section>
+  )
+}
